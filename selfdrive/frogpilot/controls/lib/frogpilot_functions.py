ec463f283d93aac3d03168e4201cea8a0e2c9ed7
reopenpilot <reopenpilot@github.com>
2024-07-29 23:00:00 +0000

    FrogAi/FrogPilot FrogPilot-Staging 2024.07.29 22:57

---
+++ selfdrive/frogpilot/controls/lib/frogpilot_functions.py
@@ -0,0 +1,205 @@
+import datetime
+import filecmp
+import glob
+import http.client
+import numpy as np
+import os
+import shutil
+import socket
+import subprocess
+import threading
+import time
+import urllib.error
+import urllib.request
+
+from openpilot.common.basedir import BASEDIR
+from openpilot.common.numpy_fast import clip, interp, mean
+from openpilot.common.params_pyx import Params, ParamKeyType, UnknownKeyName
+from openpilot.common.time import system_time_valid
+from openpilot.system.hardware import HARDWARE
+
+MODELS_PATH = "/data/models"
+
+def delete_file(file):
+  try:
+    os.remove(file)
+    print(f"Deleted file: {file}")
+  except FileNotFoundError:
+    print(f"File not found: {file}")
+  except Exception as e:
+    print(f"An error occurred: {e}")
+
+def is_url_pingable(url, timeout=5):
+  try:
+    urllib.request.urlopen(url, timeout=timeout)
+    return True
+  except (http.client.IncompleteRead, http.client.RemoteDisconnected, socket.gaierror, socket.timeout, urllib.error.HTTPError, urllib.error.URLError):
+    return False
+
+def update_frogpilot_toggles():
+  def update_params():
+    params_memory = Params("/dev/shm/params")
+    params_memory.put_bool("FrogPilotTogglesUpdated", True)
+    time.sleep(1)
+    params_memory.put_bool("FrogPilotTogglesUpdated", False)
+  threading.Thread(target=update_params).start()
+
+def run_cmd(cmd, success_msg, fail_msg):
+  try:
+    subprocess.check_call(cmd)
+    print(success_msg)
+  except subprocess.CalledProcessError as e:
+    print(f"{fail_msg}: {e}")
+  except Exception as e:
+    print(f"Unexpected error occurred: {e}")
+
+def calculate_lane_width(lane, current_lane, road_edge):
+  current_x, current_y = np.array(current_lane.x), np.array(current_lane.y)
+
+  lane_y_interp = interp(current_x, np.array(lane.x), np.array(lane.y))
+  road_edge_y_interp = interp(current_x, np.array(road_edge.x), np.array(road_edge.y))
+
+  distance_to_lane = np.mean(abs(current_y - lane_y_interp))
+  distance_to_road_edge = np.mean(abs(current_y - road_edge_y_interp))
+
+  return float(min(distance_to_lane, distance_to_road_edge))
+
+# Credit goes to Pfeiferj!
+def calculate_road_curvature(modelData, v_ego):
+  orientation_rate = np.abs(modelData.orientationRate.z)
+  velocity = modelData.velocity.x
+  max_pred_lat_acc = np.amax(orientation_rate * velocity)
+  return abs(float(max_pred_lat_acc / v_ego**2))
+
+def backup_directory(backup, destination, success_msg, fail_msg):
+  os.makedirs(destination, exist_ok=True)
+  try:
+    run_cmd(['sudo', 'cp', '-a', os.path.join(backup, '.'), destination], success_msg, fail_msg)
+  except OSError as e:
+    if e.errno == 28:
+      print("Not enough space to perform the backup.")
+    else:
+      print(f"Failed to backup due to unexpected error: {e}")
+
+def cleanup_backups(directory, limit):
+  backups = sorted(glob.glob(os.path.join(directory, "*_auto")), key=os.path.getmtime, reverse=True)
+  for old_backup in backups[limit:]:
+    subprocess.run(['sudo', 'rm', '-rf', old_backup], check=True)
+    print(f"Deleted oldest backup: {os.path.basename(old_backup)}")
+
+def backup_frogpilot(build_metadata):
+  backup_path = "/data/backups"
+  cleanup_backups(backup_path, 4)
+
+  branch = build_metadata.channel
+  commit = build_metadata.openpilot.git_commit_date[12:-16]
+
+  backup_dir = f"{backup_path}/{branch}_{commit}_auto"
+  backup_directory(BASEDIR, backup_dir, f"Successfully backed up FrogPilot to {backup_dir}.", f"Failed to backup FrogPilot to {backup_dir}.")
+
+def backup_toggles(params, params_storage):
+  for key in params.all_keys():
+    if params.get_key_type(key) & ParamKeyType.FROGPILOT_STORAGE:
+      value = params.get(key)
+      if value is not None:
+        params_storage.put(key, value)
+
+  backup_path = "/data/toggle_backups"
+  cleanup_backups(backup_path, 9)
+
+  backup_dir = f"{backup_path}/{datetime.datetime.now().strftime('%Y-%m-%d_%I-%M%p').lower()}_auto"
+  backup_directory("/data/params/d", backup_dir, f"Successfully backed up toggles to {backup_dir}.", f"Failed to backup toggles to {backup_dir}.")
+
+def convert_params(params, params_storage):
+  def convert_param(key, action_func):
+    try:
+      if params_storage.check_key(key) and params_storage.get_bool(key):
+        action_func()
+    except UnknownKeyName:
+      pass
+
+  version = 8
+
+  try:
+    if params_storage.check_key("ParamConversionVersion") and params_storage.get_int("ParamConversionVersion") == version:
+      print("Params already converted, moving on.")
+      return
+  except UnknownKeyName:
+    pass
+
+  print("Converting params...")
+  convert_param("ModelSelector", lambda: params.put_nonblocking("ModelManagement", "True"))
+  convert_param("DragonPilotTune", lambda: params.put_nonblocking("FrogsGoMooTune", "True"))
+
+  print("Params successfully converted!")
+  params_storage.put_int_nonblocking("ParamConversionVersion", version)
+
+def frogpilot_boot_functions(build_metadata, params, params_storage):
+  convert_params(params, params_storage)
+
+  while not system_time_valid():
+    print("Waiting for system time to become valid...")
+    time.sleep(1)
+
+  try:
+    backup_frogpilot(build_metadata)
+    backup_toggles(params, params_storage)
+  except subprocess.CalledProcessError as e:
+    print(f"Backup failed: {e}")
+
+def setup_frogpilot(build_metadata):
+  remount_persist = ['sudo', 'mount', '-o', 'remount,rw', '/persist']
+  run_cmd(remount_persist, "Successfully remounted /persist as read-write.", "Failed to remount /persist.")
+
+  os.makedirs("/persist/params", exist_ok=True)
+  os.makedirs(MODELS_PATH, exist_ok=True)
+
+  remount_root = ['sudo', 'mount', '-o', 'remount,rw', '/']
+  run_cmd(remount_root, "File system remounted as read-write.", "Failed to remount file system.")
+
+  frogpilot_boot_logo = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/frogpilot_boot_logo.png'
+  frogpilot_boot_logo_jpg = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/frogpilot_boot_logo.jpg'
+
+  boot_logo_location = '/usr/comma/bg.jpg'
+  boot_logo_save_location = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/original_bg.jpg'
+
+  if not os.path.exists(boot_logo_save_location):
+    shutil.copy(boot_logo_location, boot_logo_save_location)
+    print("Successfully saved original_bg.jpg.")
+
+  if filecmp.cmp(boot_logo_save_location, frogpilot_boot_logo_jpg, shallow=False):
+    os.remove(boot_logo_save_location)
+
+  if not filecmp.cmp(frogpilot_boot_logo, boot_logo_location, shallow=False):
+    run_cmd(['sudo', 'cp', frogpilot_boot_logo, boot_logo_location], "Successfully replaced bg.jpg with frogpilot_boot_logo.png.", "Failed to replace boot logo.")
+
+  if build_metadata.channel == "FrogPilot-Development":
+    subprocess.run(["sudo", "python3", "/persist/frogsgomoo.py"], check=True)
+
+def uninstall_frogpilot():
+  boot_logo_location = '/usr/comma/bg.jpg'
+  boot_logo_restore_location = f'{BASEDIR}/selfdrive/frogpilot/assets/other_images/original_bg.jpg'
+
+  copy_cmd = ['sudo', 'cp', boot_logo_restore_location, boot_logo_location]
+  run_cmd(copy_cmd, "Successfully restored the original boot logo.", "Failed to restore the original boot logo.")
+
+  HARDWARE.uninstall()
+
+class MovingAverageCalculator:
+  def __init__(self):
+    self.reset_data()
+
+  def add_data(self, value):
+    if len(self.data) == 5:
+      self.total -= self.data.pop(0)
+    self.data.append(value)
+    self.total += value
+
+  def get_moving_average(self):
+    if len(self.data) == 0:
+      return None
+    return self.total / len(self.data)
+
+  def reset_data(self):
+    self.data = []
+    self.total = 0
---
+++ selfdrive/frogpilot/frogpilot_process.py
@@ -0,0 +1,168 @@
+import datetime
+import os
+import threading
+
+from cereal import log, messaging
+from openpilot.common.params import Params
+from openpilot.common.realtime import Priority, config_realtime_process
+from openpilot.common.time import system_time_valid
+from openpilot.system.hardware import HARDWARE
+
+from openpilot.selfdrive.frogpilot.controls.frogpilot_planner import FrogPilotPlanner
+from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_functions import backup_toggles, is_url_pingable
+from openpilot.selfdrive.frogpilot.controls.lib.frogpilot_variables import FrogPilotVariables
+from openpilot.selfdrive.frogpilot.controls.lib.model_manager import DEFAULT_MODEL, DEFAULT_MODEL_NAME, download_all_models, download_model, update_models
+from openpilot.selfdrive.frogpilot.controls.lib.theme_manager import ThemeManager
+
+OFFLINE = log.DeviceState.NetworkType.none
+
+locks = {
+  "backup_toggles": threading.Lock(),
+  "download_all_models": threading.Lock(),
+  "download_model": threading.Lock(),
+  "time_checks": threading.Lock(),
+  "update_frogpilot_params": threading.Lock(),
+  "update_models": threading.Lock()
+}
+
+running_threads = {}
+
+def automatic_update_check(started, params):
+  update_available = params.get_bool("UpdaterFetchAvailable")
+  update_ready = params.get_bool("UpdateAvailable")
+  update_state_idle = params.get("UpdaterState", encoding='utf8') == "idle"
+
+  if update_ready and not started:
+    HARDWARE.reboot()
+  elif update_available:
+    os.system("pkill -SIGHUP -f system.updated.updated")
+  elif update_state_idle:
+    os.system("pkill -SIGUSR1 -f system.updated.updated")
+
+def time_checks(automatic_updates, deviceState, is_release, now, started, params, params_memory):
+  if deviceState.networkType == OFFLINE:
+    return
+
+  if not is_url_pingable("https://github.com"):
+    return
+
+  screen_off = deviceState.screenBrightnessPercent == 0
+  if automatic_updates and screen_off:
+    automatic_update_check(started, params)
+
+  update_maps(now, params, params_memory)
+
+  with locks["update_models"]:
+    update_models(is_release, params, params_memory, False)
+
+def update_maps(now, params, params_memory):
+  maps_selected = params.get("MapsSelected", encoding='utf8')
+  if maps_selected is None:
+    return
+
+  day = now.day
+  is_first = day == 1
+  is_Sunday = now.weekday() == 6
+  schedule = params.get_int("PreferredSchedule")
+
+  maps_downloaded = os.path.exists('/data/media/0/osm/offline')
+  if maps_downloaded and (schedule == 0 or (schedule == 1 and not is_Sunday) or (schedule == 2 and not is_first)):
+    return
+
+  suffix = "th" if 4 <= day <= 20 or 24 <= day <= 30 else ["st", "nd", "rd"][day % 10 - 1]
+  todays_date = now.strftime(f"%B {day}{suffix}, %Y")
+
+  if params.get("LastMapsUpdate", encoding='utf-8') == todays_date:
+    return
+
+  if params.get("OSMDownloadProgress", encoding='utf-8') is None:
+    params_memory.put_nonblocking("OSMDownloadLocations", maps_selected)
+    params.put_nonblocking("LastMapsUpdate", todays_date)
+
+def run_thread_with_lock(name, lock, target, args):
+  if name not in running_threads or not running_threads[name].is_alive():
+    with lock:
+      thread = threading.Thread(target=target, args=args)
+      thread.start()
+      running_threads[name] = thread
+
+def frogpilot_thread():
+  config_realtime_process(5, Priority.CTRL_LOW)
+
+  frogpilot_toggles = FrogPilotVariables.toggles
+
+  params = Params()
+  params_memory = Params("/dev/shm/params")
+  params_storage = Params("/persist/params")
+
+  frogpilot_planner = FrogPilotPlanner()
+  theme_manager = ThemeManager()
+
+  is_release = FrogPilotVariables.release
+  run_time_checks = False
+  started_previously = False
+  time_validated = system_time_valid()
+  update_toggles = False
+
+  pm = messaging.PubMaster(['frogpilotPlan'])
+  sm = messaging.SubMaster(['carState', 'controlsState', 'deviceState', 'frogpilotCarControl',
+                            'frogpilotCarState', 'frogpilotNavigation', 'modelV2', 'radarState'],
+                            poll='modelV2', ignore_avg_freq=['radarState'])
+
+  while True:
+    sm.update()
+
+    now = datetime.datetime.now()
+    deviceState = sm['deviceState']
+    started = deviceState.started
+
+    if not started and started_previously:
+      frogpilot_planner = FrogPilotPlanner()
+
+    if started and sm.updated['modelV2']:
+      frogpilot_planner.update(sm['carState'], sm['controlsState'], sm['frogpilotCarControl'], sm['frogpilotCarState'],
+                               sm['frogpilotNavigation'], sm['modelV2'], sm['radarState'], frogpilot_toggles)
+      frogpilot_planner.publish(sm, pm, frogpilot_toggles)
+
+    model_to_download = params_memory.get("ModelToDownload", encoding='utf-8')
+    if model_to_download:
+      run_thread_with_lock("download_model", locks["download_model"], download_model, (model_to_download, params_memory))
+
+    if params_memory.get_bool("DownloadAllModels"):
+      run_thread_with_lock("download_all_models", locks["download_all_models"], download_all_models, (params, params_memory))
+
+    if FrogPilotVariables.toggles_updated:
+      update_toggles = True
+    elif update_toggles:
+      run_thread_with_lock("update_frogpilot_params", locks["update_frogpilot_params"], FrogPilotVariables.update_frogpilot_params, (started,))
+
+      if not frogpilot_toggles.model_manager:
+        params.put_nonblocking("Model", DEFAULT_MODEL)
+        params.put_nonblocking("ModelName", DEFAULT_MODEL_NAME)
+
+      if time_validated and not started:
+        run_thread_with_lock("backup_toggles", locks["backup_toggles"], backup_toggles, (params, params_storage))
+
+      update_toggles = False
+
+    started_previously = started
+
+    if now.second == 0:
+      run_time_checks = True
+    elif run_time_checks or not time_validated:
+      run_thread_with_lock("time_checks", locks["time_checks"], time_checks, (frogpilot_toggles.automatic_updates, deviceState, is_release, now, started, params, params_memory))
+      run_time_checks = False
+
+      if not time_validated:
+        time_validated = system_time_valid()
+        if not time_validated:
+          continue
+        run_thread_with_lock("update_models", locks["update_models"], update_models, (is_release, params, params_memory))
+
+      theme_manager.update_holiday()
+
+def main():
+  frogpilot_thread()
+
+if __name__ == "__main__":
+  main()
