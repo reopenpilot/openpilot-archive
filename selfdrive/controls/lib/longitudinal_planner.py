176ce8d30bfa9c272c0b66e8a490574bbc96dec7
reopenpilot <reopenpilot@github.com>
2024-09-16 22:53:58 +0000

    FrogAi/FrogPilot FrogPilot-Staging 2024.09.16 22:52

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -174,7 +174,7 @@ class LongitudinalPlanner:
 
     return x, v, a, j
 
-  def update(self, clairvoyant_driver, clairvoyant_driver_v2, e2e_longitudinal_model, radarless_model, sm, frogpilot_toggles):
+  def update(self, clairvoyant_driver, clairvoyant_driver_v2, e2e_longitudinal_model, radarless_model, sm, tomb_raider, frogpilot_toggles):
     self.mpc.mode = 'blended' if sm['controlsState'].experimentalMode and not clairvoyant_driver else 'acc'
 
     v_ego = sm['carState'].vEgo
@@ -204,7 +204,7 @@ class LongitudinalPlanner:
     # Prevent divergence, smooth in current v_ego
     self.v_desired_filter.x = max(0.0, self.v_desired_filter.update(v_ego))
     # Compute model v_ego error
-    self.v_model_error = 0.0 if e2e_longitudinal_model else get_speed_error_clairvoyant(sm['modelV2'], v_ego) if clairvoyant_driver_v2 else get_speed_error(sm['modelV2'], v_ego)
+    self.v_model_error = 0.0 if e2e_longitudinal_model else get_speed_error_clairvoyant(sm['modelV2'], v_ego) if (clairvoyant_driver_v2 or tomb_raider) else get_speed_error(sm['modelV2'], v_ego)
 
     if force_slow_decel:
       v_cruise = 0.0
--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -39,7 +39,8 @@ def plannerd_thread():
 
   clairvoyant_driver = frogpilot_toggles.clairvoyant_driver
   clairvoyant_driver_v2 = frogpilot_toggles.clairvoyant_driver_v2
-  e2e_longitudinal_model = clairvoyant_driver or clairvoyant_driver_v2 or frogpilot_toggles.secretgoodopenpilot_model
+  tomb_raider = frogpilot_toggles.tomb_raider
+  e2e_longitudinal_model = clairvoyant_driver or clairvoyant_driver_v2 or frogpilot_toggles.secretgoodopenpilot_model or tomb_raider
   radarless_model = frogpilot_toggles.radarless_model
 
   update_toggles = False
@@ -47,7 +48,7 @@ def plannerd_thread():
   while True:
     sm.update()
     if sm.updated['modelV2']:
-      longitudinal_planner.update(clairvoyant_driver, clairvoyant_driver_v2, e2e_longitudinal_model, radarless_model, sm, frogpilot_toggles)
+      longitudinal_planner.update(clairvoyant_driver, clairvoyant_driver_v2, e2e_longitudinal_model, radarless_model, sm, tomb_raider, frogpilot_toggles)
       longitudinal_planner.publish(e2e_longitudinal_model, sm, pm)
       publish_ui_plan(sm, pm, longitudinal_planner)
 
