78c9fec063f1520f92fcc9c5b9b6ac86704b2987
reopenpilot <reopenpilot@github.com>
2024-09-21 04:13:33 +0000

    FrogAi/FrogPilot FrogPilot-Staging 2024.09.21 04:10

--- selfdrive/car/toyota/interface.py
+++ selfdrive/car/toyota/interface.py
@@ -18,10 +18,11 @@ SteerControlType = car.CarParams.SteerControlType
 class CarInterface(CarInterfaceBase):
   @staticmethod
   def get_pid_accel_limits(CP, current_speed, cruise_speed, frogpilot_toggles):
+    CCP = CarControllerParams(CP)
     if frogpilot_toggles.sport_plus:
-      return CarControllerParams.ACCEL_MIN, get_max_allowed_accel(current_speed)
+      return CCP.ACCEL_MIN, get_max_allowed_accel(current_speed)
     else:
-      return CarControllerParams.ACCEL_MIN, CarControllerParams.ACCEL_MAX
+      return CCP.ACCEL_MIN, CCP.ACCEL_MAX
 
   @staticmethod
   def _get_params(ret, candidate, fingerprint, car_fw, disable_openpilot_long, experimental_long, docs, params):
@@ -153,6 +154,11 @@ class CarInterface(CarInterfaceBase):
       ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
       ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
+
+      # Since we compensate for imprecise acceleration in carcontroller, we can be less aggressive with tuning
+      # This also prevents unnecessary request windup due to internal car jerk limits
+      if ret.flags & ToyotaFlags.RAISED_ACCEL_LIMIT:
+        tune.kiV = [0.25]
     else:
       tune.kiBP = [0., 5., 35.]
       tune.kiV = [3.6, 2.4, 1.5]
