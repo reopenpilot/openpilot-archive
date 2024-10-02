e84968cf002408a1959047d522c5d9df9e9befe5
reopenpilot <reopenpilot@github.com>
2024-10-02 10:35:32 +0000

    FrogAi/FrogPilot FrogPilot-Staging 2024.10.02 10:34

--- selfdrive/car/toyota/carstate.py
+++ selfdrive/car/toyota/carstate.py
@@ -83,7 +83,7 @@ class CarState(CarStateBase):
     # Describes the acceleration request from the PCM if on flat ground, may be higher or lower if pitched
     # CLUTCH->ACCEL_NET is only accurate for gas, PCM_CRUISE->ACCEL_NET is only accurate for brake
     # These signals only have meaning when ACC is active
-    if self.CP.flags & ToyotaFlags.RAISED_ACCEL_LIMIT:
+    if self.CP.flags & ToyotaFlags.RAISED_ACCEL_LIMIT and self.CP.carFingerprint == CAR.LEXUS_ES_TSS2:
       # Sometimes ACC_BRAKING can be 1 while showing we're applying gas already
       self.pcm_accel_net = max(cp.vl["CLUTCH"]["ACCEL_NET"], 0.0)
       if cp.vl["PCM_CRUISE"]["ACC_BRAKING"]:
@@ -281,7 +281,7 @@ class CarState(CarStateBase):
       ("STEER_TORQUE_SENSOR", 50),
     ]
 
-    if CP.flags & ToyotaFlags.RAISED_ACCEL_LIMIT:
+    if CP.flags & ToyotaFlags.RAISED_ACCEL_LIMIT and CP.carFingerprint == CAR.LEXUS_ES_TSS2:
       messages.append(("CLUTCH", 15))
 
     if CP.carFingerprint != CAR.TOYOTA_MIRAI:
